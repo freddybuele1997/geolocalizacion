351a95883a5045bdbff58c5abbcc5a0886c1ff75
freddybuele1997 <50892557+freddybuele1997@users.noreply.github.com>
2019-05-22 18:01:19 -0500

    mostrar direccion
    se convirtio las coordenadas en direccion

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, url_for, jsonify
 import urllib.request, json
+from pygeocoder import Geocoder
 
 app = Flask(__name__)
 
-jsdata = 'dsd'
+js_latitud = 'lat'
+js_longitud = 'lon'
+resultado = 'direccion'
 
 
 def public_ip():
@@ -45,15 +48,22 @@ def localizar():
 
 @app.route('/postmethod', methods=['POST'])
 def get_post_javascript_data():
-    global jsdata
-    jsdata = request.form['javascript_data']
-    print(jsdata)
-    return jsdata
+    global js_latitud
+    global js_longitud
+    js_latitud = request.form['js_latitud']
+    js_longitud = request.form['js_longitud']
+    results = Geocoder(api_key='AIzaSyCRjRkHf-0D28I2DFoZKRIyWIJJ_VoVrt4').reverse_geocode(float(js_latitud),
+                                                                                          float(js_longitud))
+
+    global coordenadas
+    coordenadas = {'direccion':results.formatted_address}
+
+    return jsonify(coordenadas)
+
 
 @app.route('/getpythondata')
 def get_python_data():
-    print(jsdata)
-    return json.dumps(jsdata)
+    return json.dumps(coordenadas)
 
 
 if __name__ == "__main__":
